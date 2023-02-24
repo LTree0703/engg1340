2be059d45316e55177f2e309fed9a12e30bb69ae
LTree0703 <108115115+LTree0703@users.noreply.github.com>
2023-02-24 13:34:34 +0800

    updated

--- Module 5/meanSD.cpp
+++ Module 5/meanSD.cpp
@@ -3,7 +3,7 @@
 # include <math.h>
 using namespace std;
 
-double cal_mean(double x1, double x2, double x3, double x4, double x5);
+double cal_mean(double x[5]);
 double cal_sd(double mean, double x[5]);
 
 int main()
@@ -12,16 +12,21 @@ int main()
     cin >> x1 >> x2 >> x3 >> x4 >> x5;
     double x[5] = {x1, x2, x3, x4, x5};
     
-    double mean = cal_mean(x1, x2, x3, x4, x5);
+    double mean = cal_mean(x);
     double sd = cal_sd(mean, x);
 
     printf("Mean = %.2f\n", mean);
     printf("Standard deviation = %.2f\n", sd);
 }
 
-double cal_mean(double x1, double x2, double x3, double x4, double x5)
+double cal_mean(double x[5])
 {
-    return (x1 + x2 + x3 + x4 + x5) / 5.0;
+    double num = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        num += x[i];
+    }
+    return (num / 5.0);
 }
 
 double cal_sd(double mean, double x[5])
@@ -32,5 +37,5 @@ double cal_sd(double mean, double x[5])
         num += (x[i]-mean) * (x[i]-mean);
         cout << num << endl;
     }
-    return sqrt(num/5.0);
+    return sqrt(num / 5.0);
 }
